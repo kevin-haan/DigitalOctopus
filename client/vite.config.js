8b57650d5bc693b39d8d0389d131444556645445
lemonblue <khaan@lemon-blue.de>
2023-12-17 19:37:17 +0100

    nginx

--- client/vite.config.js
+++ client/vite.config.js
@@ -1,5 +1,5 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
 
 // https://vitejs.dev/config/
 export default defineConfig({
@@ -10,6 +10,6 @@ export default defineConfig({
     },
     host: true, // needed for the Docker Container port mapping to work
     strictPort: true,
-    port: 80, // you can replace this port with any port
-  }
-})
+    port: 3000, // you can replace this port with any port
+  },
+});
